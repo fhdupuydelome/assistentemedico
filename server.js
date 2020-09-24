958e823f8409d74f95eda534dc7bde7f6600a4e7
Fernando Henrique lome <fernando.dupuy@gmail.com>
2020-09-24 15:08:06 -0300

    Segundo Commit

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express');
-const app = express;
+const app = express();
 
+app.listen(3000, function () {
+	console.log('Servidor rodando na porta 3000');
+});
 
+var path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname , 'views'))
+app.get('/', (req, res)=> {
+	res.render('pages/index.ejs')
+})
