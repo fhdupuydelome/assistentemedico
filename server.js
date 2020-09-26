097b64699b9a3e4df5cedae159ea23aa080681a6
Fernando Henrique lome <fernando.dupuy@gmail.com>
2020-09-26 12:49:23 -0300

    bootstrap added

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+
 app.listen(3000, function () {
 	console.log('Servidor rodando na porta 3000');
 });
 
 var path = require('path')
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname , 'views'))
 app.get('/', (req, res)=> {
-	res.render('pages/index.ejs')
+	res.render('index.ejs')
 })
