83cb2f2ba31672432f660f4fccd2b2ebf61254e4
Uitalo Souza <uitalo.urss@gmail.com>
2020-10-08 22:44:25 -0300

    Commit uitalo

--- server.js
+++ server.js
@@ -100,3 +100,149 @@ app.route('/medicos-deletar/:id').get((req, res) => {
     res.redirect('/medicos-listar');
   });
 });
+
+//rotas da view Pacientes
+app.get('/paciente', (req, res) => {
+	res.render('pages/pacientes/cadastrarPaciente.ejs');
+})
+
+app.get('/lista-paciente', (req, res) => {
+	db.collection('paciente').find().toArray((err, results) => {
+		if(err) return console.log(err);
+		res.render('pages/pacientes/listarpacientes.ejs', {data: results});
+	})
+})
+
+app.post('/salvaPaciente', (req, res) => {
+	db.collection('paciente').save(req.body, (err, result) =>{
+		if(err) return console.log(err);
+		console.log('Salvo no banco de dados');
+		res.redirect('/paciente');
+	})
+})
+
+app.route('/editarPaciente/:id').get((req, res) => {
+	var id = req.params.id;
+	db.collection('paciente').find(ObjectId(id)).toArray((err, result) => {
+		if (err) return res.send(err)
+		res.render('./pages/pacientes/editarPaciente.ejs', {data: result});
+	})
+})
+.post((req, res) => {
+	var id = req.params.id;
+	var nome = req.body.nome;
+	var sobrenome = req.body.sobrenome;
+	var cpf = req.body.cpf;
+	var telefone = req.body.telefone;
+	var logradouro = req.body.logradouro
+	var numero = req.body.numero;
+	var cidade = req.body.cidade;
+	var estado = req.body.estado;
+	db.collection('paciente').updateOne({_id: ObjectId(id)}, {
+		$set: {
+			nome: nome,
+			sobrenome: sobrenome,
+			cpf: cpf,
+			telefone: telefone,
+			logradouro: logradouro,
+			numero: numero,
+			cidade: cidade,
+			estado: estado
+		}
+	}, (err, result) => {
+		if(err) return res.send (err);
+		res.redirect('/lista-paciente');
+		console.log("Atualizado no banco de dados");
+	});
+});
+
+app.route('/deletarPaciente/:id').get((req, res) => {
+	var id = req.params.id;
+	db.collection('paciente').deleteOne({_id: ObjectId(id)}, (err, result) => {
+		if(err) return res.send(500, err);
+		res.redirect('/lista-paciente');
+		console.log('Deletado do Banco de Dados');
+		
+	});
+});
+//Fim das rotas da view Paciente
+
+// ROtas da view medicamentos medicamentos
+app.get('/cadastrarMedicamentos', function(req, res){
+    res.render('./pages/medicamentos/cadastrar.ejs');
+})
+
+app.get('/show', (req, res) => {
+  db.collection('medicamento').find().toArray((err, results) => {
+      if (err) return console.log(err)
+      console.log(results)
+      res.render('show', {medicamento: results })
+
+  })
+})
+
+
+
+app.post('/show', (req, res)=>{
+    //criar a coleção medicamento, que irá armazenar nossos dados
+    db.collection('medicamento').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+     
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/show')
+      })
+});
+
+app.route('/medicamentos/:id')
+.get((req, res) => {
+  var id = req.params.id
+    db.collection('medicamento').find(ObjectId(id)).toArray((err, result) => {
+        if (err) return res.send(err)
+        res.render('medicamentos', { medicamento: result })
+    })
+})
+
+.post((req, res) => {
+  var id = req.params.id
+  var name = req.body.name
+  var horas = req.body.horas
+  var substancia = req.body.substancia
+  var dosagem = req.body.dosagem
+  var marca = req.body.marca
+  var lab = req.body.lab
+  var tomar = req.body.tomar
+  var teste = req.body.teste
+  
+  db.collection('medicamento').updateOne({_id: ObjectId(id)}, {
+      $set: {
+        name:name,
+        horas: horas,
+        substancia:substancia,
+        dosagem: dosagem,
+        marca:marca,
+        lab: lab,
+        tomar:tomar,
+        teste: teste
+      }
+    }, (err, result) => {
+      if (err) return res.send(err)
+      
+      console.log('Banco Atualizado com Sucesso!')
+      res.redirect('/show')
+      
+    })
+})
+
+app.route('/delete/:id')
+.get((req, res) => {
+  var id = req.params.id
+
+  db.collection('medicamento').deleteOne({_id: ObjectId(id)},
+  (err, result) => {
+      if (err) return console.log(err)
+      console.log('Valor removido com Sucesso!')
+      res.redirect('/show')
+    })
+})
+
+//fim da rota pacientes
